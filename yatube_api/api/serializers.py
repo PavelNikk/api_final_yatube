e0d7ab01df1a3d0491fa1fac9a98d2d491b2b2fd
Pavel Nikulin <pashenca8@mail.ru>
2025-06-05 10:58:18 +0300

    tests pass

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,55 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
 
-
-from posts.models import Comment, Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field="username", read_only=True
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ("id", "author", "post", "text", "created")
+        read_only_fields = ("post",)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ("id", "title", "slug", "description")
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field="username", read_only=True
+    )
+
+    class Meta:
+        model = Post
+        fields = ("id", "text", "author", "pub_date", "image", "group")
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field="username",
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field="username",
+    )
+
+    class Meta:
+        model = Follow
+        fields = ("user", "following")
+
+    def validate(self, data):
+        source = self.context["request"].user
+        destination = data.get("following")
+        if source == destination:
+            raise serializers.ValidationError("Fail")
+        if Follow.objects.filter(user=source, following=destination).exists():
+            raise serializers.ValidationError("Sub already exists")
+        return data
